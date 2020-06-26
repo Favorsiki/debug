b0d3c9c91229636cdb1dcf2367c536d019e9f1b6
Favorsiki <2842121197@qq.com>
2020-06-27 07:43:53 +0800

    Debug case 10 (1.c to 5.c)

---
+++ 10/2.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,j;
+    scanf("%d",&n);
+    if(n==2) {
+        printf("2\n");
+    } else {
+        for(i = 2;i <= n;i = i + 1) {
+            for(j = 2 ;j < i ;j = j + 1) {
+                if(i % j == 0) {
+                    break;
+                }
+            }
+            if(i == j) {
+                printf("%d\n", i);
+            }
+        }
+    }
+    return 0;
+}
+    
