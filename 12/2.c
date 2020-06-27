67c7f554b4f577876ece3d19f983914dcc57800d
Favorsiki <2842121197@qq.com>
2020-06-27 10:56:53 +0800

    Debug case 12 (1.c to 5.c )

--- 12/2.c
+++ 12/2.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
-
 #define PI 3.14159
+
 int main() {
-    float r;
-    scanf("%lf\n", &r);
+    double r;
+    scanf("%lf", &r);
     printf("%.3f\n", PI * r * 2);
     printf("%.3f", PI * r * r);
     return 0;
