d7fe5356e60f00f252d3cb754555526866164c5c
Favorsiki <2842121197@qq.com>
2020-06-27 09:48:48 +0800

    Debug case 11 (1.c to 5.c)

--- 11/1.c
+++ 11/1.c
@@ -5,9 +5,9 @@ int main() {
     int arr[32] = {0};
     char input[500];
     int bits[32];
-    char result[32];
+    char result[33];
     int i = 1, j = 0;
-    while (scanf("%c\n", &input[i - 1]) != EOF) {
+    while (scanf("%c", &input[i - 1]) != EOF && input[i-1] != '\n') {
         arr[i % 32] = arr[i % 32] + (int)input[i - 1];
         i++;
     }
@@ -19,7 +19,7 @@ int main() {
     for (j = 0; j <= 31; j++) {
         result[j] = (bits[j] % 85) + 34;    
     }
-    
+    result[32] = '\0';
     puts(result);
     return 0;
 }
