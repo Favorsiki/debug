670a73168b45bbdfc46d472b6ec9e932d1c000c6
Favorsiki <2842121197@qq.com>
2020-06-28 08:16:57 +0800

    Debug case 11 (6.c to 10c)

--- 11/4.c
+++ 11/4.c
@@ -8,10 +8,10 @@ int f(char k[500]) {
     int i;
     int j;
     int m = strlen(k);
-    for (i = 1;i <= m;i++) {//(1<=i<=k)
-        arr[i%32] = arr[i % 32] + k[i - 1];   
+    for (i = 1;i <= m;i++) {
+        arr[i % 32] = arr[i % 32] + k[i - 1];   
     }
-    for (j = 0;j <= 31;j++) {//(0<=j<=31)
+    for (j = 0;j <= 31;j++) {
         bits[j] = arr[31-j] ^ (arr[j] << 1);
         printf("%c", bits[j] % 85 + 34 );
     }
