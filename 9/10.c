d53603fe0734e1fb2deeba88a9cc444fa47aa2b6
liruijie <402443527@qq.com>
2020-06-28 13:36:44 +0800

    9-finished

--- 9/10.c
+++ 9/10.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 int f(int n){
-    if(n<=3){return 1;}
-    else{return f(n-2)+f(n-3);}
+    if (n <= 4) {
+        return 1;
+    } else {
+        return f(n-2)+f(n-3);
+    }
 }
 int main() {
     int n;
---
+++ 9/7.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int i,N,x;
+    int a[51] = {0};
+    scanf("%d",&N);
+    a[2] = 1;
+    a[3] = 1;
+    a[4] = 1;
+    if (N <= 4) {
+	    printf("%d",a[N]);
+    } else {
+        for (i = 5; i < N; i++) {
+            a[i] = a[i - 2] + a[i - 3];
+        }
+        printf("%d",a[N-2]+a[N-3]);
+    }
+    return 0;
+}
