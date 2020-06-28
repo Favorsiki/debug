c61d9f256a63a64ae2bee41eaf53a35862ec0035
liruijie <402443527@qq.com>
2020-06-28 10:00:00 +0800

    7-finished

---
+++ 7/8.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+int main()
+{
+    int y,m,d;
+    scanf("%d-%d-%d",&y,&m,&d);
+    if (m == 1 || m == 3 || m == 5 || m == 7 || m == 8 || m == 10 || m == 12) {
+        if (d <= 31) {
+            printf("YES");
+        } else {
+            printf("NO");
+        }
+    } else if (m == 4 || m == 6 || m == 9 || m == 11) {
+        if (d <= 30) {
+            printf("YES");
+        } else {
+            printf("NO");
+        }
+    } else if(m < 1 || m > 12) {
+    	printf("NO");
+	} else {
+        if ((y % 100 != 0 && y % 4 == 0) || y % 400 == 0) {
+            if (d <= 29) {
+                printf("YES");
+            } else {
+                printf("NO");
+            }
+        } else {
+            if(d <= 28) {
+                printf("YES");
+            } else {
+                printf("NO");
+            }
+        }    
+    }
+    return 0;
+}
