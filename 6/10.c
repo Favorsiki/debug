00335f3fb986331328e615bdf7b43b52d4c95856
liruijie <402443527@qq.com>
2020-06-30 15:36:01 +0800

    OK

--- 6/10.c
+++ 6/10.c
@@ -1,57 +1,67 @@
-#include <string.h>
 #include <stdio.h>
-int main()
-{
-    char pms[4];
-    char change_pms[100][3];
-    int i;
-    i = 0;
-    int j;
-    j = 0;
-    
-    int a;
-    a = 0;
-    int b;
-    b = 0;
-    int c;
-    c = 0;
-
-
-    scanf("%s\n", pms);
-
-    while (scanf("%s\n",change_pms[i++]) != EOF) {
-        ;
-    }
-
-    for (i = 0; i < strlen(pms); i++) {
-        if (pms[i] == 'r') {
-            a = 1;
-        } else if (pms[i] == 'w') {
-            b = 1;
+void add(char);
+void dele(char);
+int rwx = 0;
+int main() {
+    char temp;
+    scanf("%c", &temp);
+    if (temp == 'x') {
+            rwx ^= (1 << 0);
+    } else if (temp == 'w') {
+        rwx ^= (1 << 1);
+        scanf("%c", &temp);
+        if (temp == 'x') {
+                    rwx ^= (1 << 0);
         } else {
-            c = 1;
+            goto begin;
+        }
+    }
+    else if (temp == 'r') {
+        rwx ^= (1 << 2);
+        scanf("%c", &temp);
+        if (temp == 'w') {
+                    rwx ^= (1 << 1);
+        } else {
+            goto begin;
+        }
+        scanf("%c", &temp);
+        if (temp == 'x') {
+                    rwx ^= (1 << 0);
+        } else {
+            goto begin;
         }
     }
 
-    for (i = 0; i < strlen(change_pms[i]); i++) {
-        if (change_pms[i][j] == '+') {
-            if (change_pms[i][j + 1] == 'r') {
-                a = 1;
-            } else if (change_pms[i][j + 1] == 'w') {
-                b = 1;
-            } else {
-                c = 1;
-            }
-        } else {
-            if (change_pms[i][j + 1] == 'r') {
-                a = 0;
-            } else if (change_pms[i][j + 1] == 'w') {
-                b = 0;
-            } else {
-                c = 0;
-            }
+    while (scanf("%c", &temp) != EOF) {
+        begin:
+        if (temp == '+') {
+            scanf("%c", &temp);
+            add(temp);
+        } else if (temp == '-') {
+            scanf("%c", &temp);
+            dele(temp);
         }
     }
-    printf("%d", a * 4 + b * 2 + c);
+
+    printf("%d", rwx);
     return 0;
+}
+void add(char input) {
+    if (input == 'r' && rwx / 2 / 2 != 1) {
+        rwx ^= (1 << 2);
+        } else if (input == 'w' && (rwx / 2 % 2 != 1)) {
+        rwx ^= (1 << 1);
+        } else if (rwx % 2 != 1) {
+        rwx ^= (1 << 0);
+        }
+}
+void dele(char input)
+{
+    if (input == 'r' && rwx / 2 / 2 == 1) {
+        rwx ^= (1 << 2);
+        } else if (input == 'w' && (rwx / 2 % 2 == 1)) {
+                rwx ^= (1 << 1);
+        } else if (rwx % 2 == 1) {
+                rwx ^= (1 << 0);
+        }
 }
