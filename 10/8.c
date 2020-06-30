a2e78f2e1cd6b18fb4396589c10aafa48b3822ea
Favorsiki <2842121197@qq.com>
2020-06-30 15:54:01 +0800

    Final debug 10

--- 10/8.c
+++ 10/8.c
@@ -3,32 +3,31 @@ int fun (int n) {
     int i,j,num;
     for (i = 1; i <= n; i++) {
         for (j = 2; j <= i; j++) {
-	    if (i % j == 0) {
-		break;
-	    }
-	}
-	if (j == i) {
-	    num = i;
-	}
+            if (i % j == 0) {
+                break;
+            }
+        }
+        if (j == i) {
+            num = i;
+        }
     }
     return num;
 }
 
-int main(){
-    int n,i,j;
-    printf("Please enter range:");
+int main() {
+    int n, i, j;
     scanf("%d",&n);
     for (i = 1; i <= n; i++) {
         for (j = 2; j <= i; j++) {
-	    if (i % j == 0) {
-		break;
-	    }
-	}
-	if (j == i && i != fun(n)) {
-	    printf("%d\n",i);
-	} else if (j == i && i == fun(n)) {
-	    printf("%d",i);
-	}
-    }	
+            if (i % j == 0) {
+                break;
+            }
+        }
+        if (j == i && i != fun(n)) {
+            printf("%d\n",i);
+        } else if (j == i && i == fun(n)) {
+            printf("%d",i);
+        }
+    }    
     return 0;
 }
